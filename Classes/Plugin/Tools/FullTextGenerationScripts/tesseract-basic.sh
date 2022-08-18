1685b59b6eb520caf656db5d5f2a1a13aac7428c
Christos Sidiropoulos <csidirop@runbox.com>
2022-08-18 15:25:20 +0200

    Add parameter descriptions
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/tesseract-basic.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/tesseract-basic.sh
@@ -16,16 +16,16 @@ function test() {
 while [ $# -gt 0 ] ; do
   case $1 in
   	-h | --help) 		usage;;
-	--page_id) 			page_id="$2" ;;
-	--image_path) 		image_path="$2" ;;
-	--image_URL) 		image_URL="$2" ;;
-	--doc_path)			doc_path="$2" ;;
-	--xml_path)			xml_path="$2" ;;
-	--temp_xml_path)	temp_xml_path="$2" ;;
-	--lock_folder)		lock_folder="$2" ;;
-	--image_download_command) image_download_command="$2" ;;
-	--ocrLanguages) 	ocrLanguages="$2" ;;
-	--ocrOptions)		ocrOptions="$2" ;;
+	--page_id) 			page_id="$2" ;;			#Page number
+	--image_path) 		image_path="$2" ;;		#Imagefile path
+	--image_URL) 		image_URL="$2" ;;		#Image URL
+	--doc_path)			doc_path="$2" ;;		#Fulltextfolder path
+	--xml_path)			xml_path="$2" ;;		#Fulltextfile path
+	--temp_xml_path)	temp_xml_path="$2" ;;	#Fulltextfile TMP path
+	--lock_folder)		lock_folder="$2" ;;		#Folder used to lock ocr command
+	--image_download_command) image_download_command="$2" ;; #wget image and save to $image_path
+	--ocrLanguages) 	ocrLanguages="$2" ;;	#Models&Languages for Tesseract
+	--ocrOptions)		ocrOptions="$2" ;;		#Output types
 	--test)				test ;;
   esac
   shift
