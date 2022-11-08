35cf954298193d95c9503bafb839aedd94da71e1
Christos Sidiropoulos <csidirop@runbox.com>
2022-11-08 16:56:41 +0100

    Find URN additionaly in "mets:div" tag
    This is necessary for some METS sources, which do not have there URNs stored elsewhere.
    
    https://github.com/UB-Mannheim/kitodo-presentation-docker/issues/32
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/FullTextGenerator.php
+++ Classes/Plugin/FullTextGenerator.php
@@ -78,6 +78,8 @@ class FullTextGenerator {
         $urn = $reader->readString();
       } else if (($reader->name=="mods:recordIdentifier") && ($reader->getAttribute("source") === 'urn') && !empty($reader->readString())){ //if XML key is 'mods:recordIdentifier' and attribute 'source' is 'urn'
         $urn = $reader->readString();
+      } else if (($reader->name=="mets:div") && (substr($reader->getAttribute("CONTENTIDS"),0,3) === 'urn') && !empty($reader->readString())){ //if XML key is under '<mets:structMap TYPE="LOGICAL">' with '"mets:div' and attribute is 'CONTENTIDS' and starts with 'urn'
+        $urn = $reader->getAttribute("CONTENTIDS");
       }
     }
     return $urn;
