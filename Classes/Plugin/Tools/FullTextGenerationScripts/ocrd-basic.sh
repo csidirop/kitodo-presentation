2816a4239b2cbbf7e2a6005a0174b2b638fc9125
Christos Sidiropoulos <csidirop@runbox.com>
2022-12-02 14:14:05 +0100

    Update OCRD script
     - add variable
     - fix typo in command
     - clean working dir
    
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/ocrd-basic.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/ocrd-basic.sh
@@ -36,18 +36,20 @@ done
 
 # Check for required parameters:
 if [[ -z ${image_path} || -z ${output_path} || -z ${ocrLanguages} || -z ${ocrOptions} ]] ; then
-  echo "Missing parameter"
-  exit 1
+	usage;
 fi
 
 # Parse URL or Path and run OCR:
 regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]' #Regex for URL validation ( https://stackoverflow.com/a/3184819 )
 if [[ (${image_path} =~ $regex) || (-f ${image_path}) ]] ; then # If image_path is a valid URL or a local file
 	echo "Running OCRD-OCR: tesseract $image_path $output_path -l $ocrLanguages $ocrOptions"
+	jobname="test";
 	# start job:
 	ssh ${ocrd-kitodo} wget -P "/data/${jobname}/images/" "${image_path}" && for_production.sh --proc-id 1 --lang deu --script Fraktur "${jobname}"
 	# get fulltexts:
-	ssh ${ocrd-kitodo}:"/data/${jobname}/ocr/alto/${filename}.xml" "${output_path}"
+	scp ${ocrd-kitodo}:"/data/${jobname}/ocr/alto/${filename}.xml" "${output_path}"
+	# clean dir:
+	ssh ${ocrd-kitodo} rm -r "/data/${jobname}/"
 	exit 0
 else
 	echo "File not found: ${image_path}"
