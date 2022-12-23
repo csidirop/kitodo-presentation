ded8bb434f43f9c6f95f6d3015aec1cd274af7dd
Christos Sidiropoulos <csidirop@runbox.com>
2022-12-23 14:42:02 +0000

    Add support for kraken
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

---
+++ Classes/Plugin/Tools/FullTextGenerationScripts/kraken-basic.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+### Kraken OCR generation script ###
+# This script provides an uniform way to run OCR with Kraken on local or remote images
+
+set -euo pipefail # exit on: error, undefined variable, pipefail
+
+# Error message:
+function usage() {
+	echo "No parameter set";
+	exit 1;
+}
+
+# Test fuction, for manually testing the script
+function test() {
+	wget https://digi.bib.uni-mannheim.de/fileadmin/digi/1652998276/max/1652998276_0001.jpg
+	kraken -i 1652998276_0001.jpg 1652998276_0001.txt segment -bl ocr
+	exit 0;
+}
+
+# Check for parameter:
+[ $# -eq 0 ] && usage # If no parameter given call usage()
+
+# Paramaters:
+while [ $# -gt 0 ] ; do
+  case $1 in
+	-h | --help)		usage ;;
+	--page_id)			page_id="$2" ;;			#Page number
+	--image_path)		image_path="$2" ;;		#Image path/URL
+	--output_path)		output_path="$2" ;;		#Fulltextfile path
+	--ocrLanguages)		ocrLanguages="$2" ;;	#Models&Languages for Kraken
+	--ocrOptions)		ocrOptions="$2" ;;		#Output types
+	--test)				test ;;
+  esac
+  shift
+done
+
+# Check for required parameters:
+if [[ -z ${image_path} || -z ${output_path} || -z ${ocrLanguages} || -z ${ocrOptions} ]] ; then
+  echo "Missing parameter" #TODO
+  exit 1
+fi
+
+# Parse URL or Path and run tesseract:
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]' #Regex for URL validation ( https://stackoverflow.com/a/3184819 )
+if [[ (${image_path} =~ $regex) || (-f ${image_path}) ]] ; then # If image_path is a valid URL or a local file
+	echo "Running OCR: kraken"
+	kraken -i ${image_path} $output_path.txt segment -bl ocr
+
+	exit 0
+else
+	echo "File not found: ${image_path}"
+	exit 2
+fi
