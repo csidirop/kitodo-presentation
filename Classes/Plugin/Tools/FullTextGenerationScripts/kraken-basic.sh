e30eb0e36caba4b3964a0104840f2aa12eb9444b
Christos Sidiropoulos <csidirop@runbox.com>
2023-01-11 14:40:41 +0000

    Update kraken script
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/kraken-basic.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/kraken-basic.sh
@@ -14,7 +14,7 @@ function usage() {
 # Test fuction, for manually testing the script
 function test() {
 	wget https://digi.bib.uni-mannheim.de/fileadmin/digi/1652998276/max/1652998276_0001.jpg
-	kraken -i 1652998276_0001.jpg 1652998276_0001.txt segment -bl ocr
+	kraken -a -i 1652998276_0001.jpg 1652998276_0001.txt binarize segment ocr -m /opt/kraken_models/digitue_best.mlmodel
 	exit 0;
 }
 
@@ -45,8 +45,10 @@ fi
 regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]' #Regex for URL validation ( https://stackoverflow.com/a/3184819 )
 if [[ (${image_path} =~ $regex) || (-f ${image_path}) ]] ; then # If image_path is a valid URL or a local file
 	echo "Running OCR: kraken"
-	kraken -i ${image_path} $output_path.txt segment -bl ocr
-
+	filename=$(basename ${image_path}) # extract filename from url
+	wget ${image_path} -P fileadmin/_temp_/ocrTempFolder/images/
+	kraken -a -i "fileadmin/_temp_/ocrTempFolder/images/${filename}" "${output_path}.xml" binarize segment ocr
+	rm "fileadmin/_temp_/ocrTempFolder/images/${filename}"
 	exit 0
 else
 	echo "File not found: ${image_path}"
