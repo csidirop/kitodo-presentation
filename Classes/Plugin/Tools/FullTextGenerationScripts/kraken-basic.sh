76e2707319fdd01602a2f369d82e5563f1cbde33
Christos Sidiropoulos <csidirop@runbox.com>
2023-06-01 09:23:13 +0000

    Update kraken-basic
     - make it work without custom model
     - do not binarize
    
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/kraken-basic.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/kraken-basic.sh
@@ -8,8 +8,7 @@ set -euo pipefail # exit on: error, undefined variable, pipefail
 # Test fuction, for manually testing the script
 function test() {
 	wget https://digi.bib.uni-mannheim.de/fileadmin/digi/1652998276/max/1652998276_0001.jpg
-	kraken --alto --input 1652998276_0001.jpg 1652998276_0001.txt binarize segment ocr --model /opt/kraken_models/digitue.mlmodel 
-	#TODO: use default model, not digitue
+	kraken --alto --input 1652998276_0001.jpg 1652998276_0001_basic.xml segment --baseline ocr
 	exit 0;
 }
 
@@ -31,11 +30,13 @@ done
 # Parse URL or Path and run tesseract:
 regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]' #Regex for URL validation ( https://stackoverflow.com/a/3184819 )
 if [[ (${image_path} =~ $regex) || (-f ${image_path}) ]] ; then # If image_path is a valid URL or a local file
-	echo "Running OCR: kraken"
+	echo "Running OCR: kraken-basic"
+
 	filename=$(basename ${image_path}) # extract filename from url
 	wget ${image_path} -P fileadmin/_temp_/ocrTempFolder/images/
-	kraken --alto --input "fileadmin/_temp_/ocrTempFolder/images/${filename}" "${output_path}.xml" binarize segment ocr
+	kraken --alto --input "fileadmin/_temp_/ocrTempFolder/images/${filename}" "${output_path}.xml" segment --baseline ocr
 	rm "fileadmin/_temp_/ocrTempFolder/images/${filename}"
+
 	exit 0
 else
 	echo "File not found: ${image_path}"
