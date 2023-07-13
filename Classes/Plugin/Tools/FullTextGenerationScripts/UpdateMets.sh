63952fa7d58714176f97ceaeb38e323da0a77a86
Christos Sidiropoulos <csidirop@runbox.com>
2023-07-13 14:54:10 +0000

    Update mets updater script
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
@@ -5,39 +5,29 @@
 
 set -euo pipefail # exit on: error, undefined variable, pipefail
 
-# Test fuction, for manually testing the script
-function test() {
-
-}
-
-
 # Paramaters:
 while [ $# -gt 0 ] ; do
   case $1 in
 	--pageId)			pageId="$2" ;;			#Page ID (eg. log59088_1)
-	--pageNum)			pageNum="$2" ;;			#Page number (eg. 1)
 	--URL)				URL="$2" ;;				#URL
 	--outputPath)		outputPath="$2" ;;		#Fulltextfile path
-	--tmpImagePath)		tmpImagePath="$2" ;;	#Temporarily image path
-	--test)				test ;;
   esac
   shift
 done
 
-
 # UPDATE METS:
-
-mv NAME mets.xml
-
+docLocalId=$(rev <<< "$pageId" | cut -d _ -f 2- | rev) # (eg. log59088)
+pageNum=$(rev <<< "$pageId" | cut -d _ -f 1 | rev) # (eg. 1)
+outputFolder=$(rev <<< "$outputPath" | cut -d / -f 2- | rev)
 #temp
 URL="http://localhost/fileadmin/fulltextFolder//URN/nbn/de/bsz/180/digosi/30/kraken-german_print"
-pageNum = 999
 #/temp
 
-getDocLocalId = $(cut -d _ -f 1 <<< $pageID)
+cd $outputFolder
+mv $docLocalId.xml mets.xml
 
-ocrd --log-level INFO workspace add -G FULLTEXT --file-id "fulltext-$pageId" --page-id="$pageNum" --mimetype text/xml "$pageId.xml"
+ocrd --log-level INFO workspace add --file-grp FULLTEXT --file-id "fulltext-$pageId" --page-id="$pageNum" --mimetype text/xml "$pageId.xml"
 perl -pi -e 's/LOCTYPE="OTHER" OTHERLOCTYPE="FILE"/LOCTYPE="URL"/' mets.xml
 perl -pi -e s,'"log59088_','"'$URL'/log59088_', mets.xml
 
-mv mets.xml NAME
+mv mets.xml $docLocalId.xml
