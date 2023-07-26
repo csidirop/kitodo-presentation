c44a33338c2cb33f42dd658bf43df2c5839e5bc1
Christos Sidiropoulos <csidirop@runbox.com>
2023-07-26 09:50:46 +0000

    Extract METS node from OAI-wrapper
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
@@ -26,8 +26,27 @@ URL="http://localhost/fileadmin/fulltextFolder//URN/nbn/de/bsz/180/digosi/30/kra
 cd $outputFolder
 mv $docLocalId.xml mets.xml
 
+# Check if METS-data is wrapped in an OAI node:
+set +euo pipefail # unset error exits
+grep -qzo '<OAI-PMH.*>.*</OAI-PMH>' mets.xml
+oai=$?
+set -euo pipefail # re set error exits
+if [ $oai ] ; then
+  # Extract the inner mets node from the OAI wrapper, make it valid xml and pretty print it:
+    # xmlstarlet sel -t -c "//mets:mets" mets.xml > mets_tmp1.xml
+    # echo '<?xml version="1.0" encoding="utf-8"?>' | cat - mets_tmp1.xml > mets_tmp2.xml
+    # xmllint --format mets_tmp2.xml
+  (echo '<?xml version="1.0" encoding="utf-8"?>' && xmlstarlet sel -t -c "//mets:mets" mets.xml) | xmllint --format - > mets_tmp.xml
+  mv mets_tmp.xml mets.xml
+fi
+
+# Update METS with given ALTO file:
 ocrd --log-level INFO workspace add --file-grp FULLTEXT --file-id "fulltext-$pageId" --page-id="$pageNum" --mimetype text/xml "$pageId.xml"
 perl -pi -e 's/LOCTYPE="OTHER" OTHERLOCTYPE="FILE"/LOCTYPE="URL"/' mets.xml
-perl -pi -e s,'"log59088_','"'$URL'/log59088_', mets.xml
+perl -pi -e s,'"log59088_','"'$URL'/log59088_', mets.xml #todo log59088 string
+
+# Validate METS:
+#apt -y install libxml2-utils
+#xmllint --noout --schema http://www.loc.gov/standards/mets/mets.xsd mets.xml
 
 mv mets.xml $docLocalId.xml
