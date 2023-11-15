78bffaab8b7fd78140dfb22e6701e2808599f07a
Christos Sidiropoulos <csidirop@runbox.com>
2023-11-15 15:25:52 +0000

    Use only xmlstarlet
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
@@ -53,8 +53,10 @@ fi
 # Update METS with given ALTO file:
 # mm-update add-file -G FULLTEXT -m text/xml -u $url $pageId.xml  # Add ALTO file to METS via mm-update
 ocrd --log-level INFO workspace add --force --file-grp FULLTEXT --file-id "fulltext-$pageId" --page-id="$pageNum" --mimetype text/xml "$pageId.xml"
-sed -i 's/LOCTYPE="OTHER" OTHERLOCTYPE="FILE"/LOCTYPE="URL"/' mets.xml # Replace LOCTYPE OTHER with URL
-sed -i s,"\"$pageId.xml","\"$url", mets.xml # Replace ALTO file path with URL
+# sed -i 's/LOCTYPE="OTHER" OTHERLOCTYPE="FILE"/LOCTYPE="URL"/' mets.xml # Replace LOCTYPE OTHER with URL #TODO not needed anymore with ocrd update?
+xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -u "//mets:file[@ID='fulltext-$pageId']/mets:FLocat/@LOCTYPE" -v "URL" -d "//mets:file[@ID='fulltext-$pageId']/mets:FLocat/@OTHERLOCTYPE" mets.xml # Replace LOCTYPE=OTHER with URL #TODO not needed anymore with ocrd update?
+# sed -i s,"\"$pageId.xml","\"$url", mets.xml # Replace ALTO file path with URL
+xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -u "//mets:file[@ID='fulltext-$pageId']/mets:FLocat/@xlink:href" -v "$url" mets.xml
 xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -a "//mets:file[@ID='fulltext-$pageId']" -t attr -n "CREATED" -v "$(date +%Y-%m-%dT%H:%M:%S%z)" mets.xml # Add Date attribute to file node
 xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -a "//mets:file[@ID='fulltext-$pageId']" -t attr -n "SOFTWARE" -v "DFG-Viewer-OCR-On-Demand-$ocrEngine" mets.xml # Add OCR-ENGINE attribute to file node
 
