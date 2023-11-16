3d5ab5a3afe4e96a6d46120f50e2661ed66f1f46
Christos Sidiropoulos <csidirop@runbox.com>
2023-11-16 15:21:49 +0000

    Properly handle original, edited or not exiting FT entries
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
+++ Classes/Plugin/Tools/FullTextGenerationScripts/UpdateMets.sh
@@ -52,27 +52,24 @@ fi
 
 # Check if there is already a FULLTEXT section for the given pageId:
 # 1. Get all FILEIDs from structMap for given page number:
-fileIdList=($(xmlstarlet sel -N x="http://www.loc.gov/METS/" -t -m '//mets:structMap[@TYPE="PHYSICAL"]/mets:div/mets:div[@ORDER='$pageNum']/mets:fptr' -v "@FILEID" -n mets.xml ));
+physID=$(xmlstarlet sel -N mets="http://www.loc.gov/METS/" -t -v "//mets:div[@ORDER='$pageNum']/@ID" mets.xml)
+fileIdList=($(xmlstarlet sel -N mets="http://www.loc.gov/METS/" -t -v '//mets:structMap[@TYPE="PHYSICAL"]/mets:div/mets:div[@ID="'$physID'"]/mets:fptr/@FILEID' -n mets.xml ));
 # 2. Check if there is already a FULLTEXT section for the given fileId:
 updated=0 # Flag to check if METS was updated
 for fileId in "${fileIdList[@]}"; do
-  if [[ -n $(xmlstarlet sel -N x="http://www.loc.gov/METS/" -t -m '//mets:fileSec/mets:fileGrp[@USE="FULLTEXT"]/mets:file[@ID="'$fileId'"]/mets:FLocat' -v "@xlink:href" -n mets.xml) ]] ; then
-    echo "Found FULLTEXT section for fileId: $fileId"; #TODO  remove
+  if [[ -n $(xmlstarlet sel -N mets="http://www.loc.gov/METS/" -t -v '//mets:fileSec/mets:fileGrp[@USE="FULLTEXT"]/mets:file[@ID="'$fileId'"]/mets:FLocat/@xlink:href' -n mets.xml) ]] ; then
     updated=1 # Set flag to 1
 
-    # Update METS by updating exisiting elements with given ALTO file:
-    # todo ocrd
-    ocrd --log-level INFO workspace add --force --file-grp FULLTEXT --file-id "$pageId" --page-id="$pageNum" --mimetype text/xml "$url"
+    # Update METS by updating existing elements with given ALTO file:
+    ocrd --log-level INFO workspace add --force --file-grp FULLTEXT --file-id "$fileId" --page-id="$physID" --mimetype text/xml "$url"
     xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -u "//mets:file[@ID='$fileId']/@CREATED" -v "$(date +%Y-%m-%dT%H:%M:%S%z)" -i "//mets:file[@ID='$fileId'][not(@CREATED)]" -t attr -n "CREATED" -v "$(date +%Y-%m-%dT%H:%M:%S%z)" mets.xml  # Add/Update date attribute to file node
     xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -u "//mets:file[@ID='$fileId']/@SOFTWARE" -v "DFG-Viewer-OCR-On-Demand-$ocrEngine" -i "//mets:file[@ID='$fileId'][not(@SOFTWARE)]" -t attr -n "SOFTWARE" -v "DFG-Viewer-OCR-On-Demand-$ocrEngine" mets.xml  # Add OCR-ENGINE attribute to file node
   fi
-   echo "$fileId"; # TODO remove
 done
 
 if [[ $updated == 0 ]]; then # No FULLTEXT section for fileId
-  echo "No FULLTEXT section for fileId: $fileId"; # TODO remove
   # Update METS by adding given ALTO file:
-  ocrd --log-level INFO workspace add --force --file-grp FULLTEXT --file-id "fulltext-$pageId" --page-id="fulltext-$pageNum" --mimetype text/xml "$url"
+  ocrd --log-level INFO workspace add --force --file-grp FULLTEXT --file-id "fulltext-$pageId" --page-id="$physID" --mimetype text/xml "$url"
   xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -a "//mets:file[@ID='fulltext-$pageId']" -t attr -n "CREATED" -v "$(date +%Y-%m-%dT%H:%M:%S%z)" mets.xml # Add Date attribute to file node
   xmlstarlet ed -L -N mets="http://www.loc.gov/METS/" -a "//mets:file[@ID='fulltext-$pageId']" -t attr -n "SOFTWARE" -v "DFG-Viewer-OCR-On-Demand-$ocrEngine" mets.xml # Add OCR-ENGINE attribute to file node
   # TODO add `OTHER` attribute to file node as soon as ocrd gets updated
